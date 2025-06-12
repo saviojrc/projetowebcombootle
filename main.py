619a033cf403039794c4f774c8830b4f5c2504ed
Savio Jailson Rodrigues Costa <saviojrc.1988@gmail.com>
2025-06-12 12:14:33 -0300

    20250612_1224

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.app import Aplication
+
+if __name__ == '__main__':
+    Aplication.run()
---
+++ src/app.py
@@ -0,0 +1,62 @@
+from bottle import route, run
+from bottle import request, template
+from bottle import static_file, get
+from bottle import error
+
+# static routes
+class Aplication:
+	@staticmethod
+	@get('/<filename:re:.*\.css>')
+	def stylesheets(filename):
+		return static_file(filename, root='static/css')
+
+	@staticmethod
+	@get('/<filename:re:.*\.js>')
+	def javascripts(filename):
+		return static_file(filename, root='static/js')
+
+	@staticmethod
+	@get('/<filename:re:.*\.(jpg|png|gif|ico)>')
+	def images(filename):
+		return static_file(filename, root='static/img')
+
+	@staticmethod
+	@get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
+	def fonts(filename):
+		return static_file(filename, root='static/fonts')
+
+	@staticmethod
+	@route('/login')
+	@get('/')
+	def login():
+		return template('login')
+
+	@staticmethod
+	def check_login(username, password):
+		d = {'marcos': 'python', 'joao': 'java', 'pedro': 'go'}
+		if username in d.keys() and d[username] == password:
+			return True
+		return False
+
+	@staticmethod
+	@route('/login', method='POST')  # @post('/login')
+	def acao_login():
+		username = request.forms.get('username')
+		password = request.forms.get('password')
+
+		if not Aplication.check_login(username, password):
+			return template('erro_login', sucesso=False, nome=username)
+
+		return template('sucesso_login', nome=username)
+
+	@staticmethod
+	@error(404)
+	def error404(erro):
+		print(erro)
+		return template('pagina404')
+
+	@staticmethod
+	def run():
+		run(host='localhost', port=8080, debug=True, reloader=True)
+
+
