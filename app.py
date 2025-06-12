cb215be753fb2baad0e3130c23ddf3273f3a1c50
Savio Jailson Rodrigues Costa <saviojrc.1988@gmail.com>
2025-06-12 11:12:31 -0300

    20250612_1112

--- app.py
+++ app.py
@@ -1,60 +1,45 @@
+from bottle import route, run
+from bottle import request, template
+from bottle import static_file, get
+from bottle import error
 
-from model.enums.redirecionamento_enum import RedirecionamentoEnum, get_route_path
-from venv.Scripts.bottle import static_file, get, route, template, request, error, run
+# static routes
+@get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+	return static_file(filename, root='static/css')
 
+@get('/<filename:re:.*\.js>')
+def javascripts(filename):
+	return static_file(filename, root='static/js')
 
-class Aplication:
-	# static routes
-	@staticmethod
-	@get('/<filename:re:.*\.css>')
-	def stylesheets(filename):
-		return static_file(filename, root=get_route_path(RedirecionamentoEnum.STYLESHEETS))
+@get('/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+	return static_file(filename, root='static/img')
 
-	@staticmethod
-	@get('/<filename:re:.*\.js>')
-	def javascripts(filename):
-		return static_file(filename, root=get_route_path(RedirecionamentoEnum.JAVASCRIPTS))
+@get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
+def fonts(filename):
+	return static_file(filename, root='static/fonts')
 
-	@staticmethod
-	@get('/<filename:re:.*\.(jpg|png|gif|ico)>')
-	def images(filename):
-		return static_file(filename, root=get_route_path(RedirecionamentoEnum.IMAGES))
+@route('/login') # @get('/login')
+def login():
+	return template('login')
 
-	@staticmethod
-	@get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
-	def fonts(filename):
-		return static_file(filename, root=get_route_path(RedirecionamentoEnum.FONTS))
+def check_login(username, password):
+	d = {'marcos':'python', 'joao':'java', 'pedro':'go'}
+	if username in d.keys() and d[username] == password:
+		return True
+	return False
 
-	@staticmethod
-	@route('/login')  # @get('/login')
-	def login():
-		return template(get_route_path(RedirecionamentoEnum.LOGIN))
+@route('/login', method='POST') # @post('/login')
+def acao_login():
+	username = request.forms.get('username')
+	password = request.forms.get('password')
+	return template('verificacao_login', sucesso=check_login(username, password), nome=username)
 
-	@staticmethod
-	def check_login(username, password):
-		d = {'marcos': 'python', 'joao': 'java', 'pedro': 'go'}
-		if username in d.keys() and d[username] == password:
-			return True
-		return False
+@error(404)
+def error404(erro):
+	print(erro)
+	return template('pagina404')
 
-	@staticmethod
-	@route('/login', method='POST')  # @post('/login')
-	def acao_login():
-		username = request.forms.get('username')
-		password = request.forms.get('password')
-		if Aplication.check_login(username, password):
-			return template(get_route_path(RedirecionamentoEnum.SUCESSO_LOGIN), username=username)
-		else:
-			return template(get_route_path(RedirecionamentoEnum.LOGIN_COM_FALHA))
-
-	## Metodo que trata o erro 404
-	@staticmethod
-	@error(404)
-	def erro_404(erro):
-		print(str(erro))
-		return template(get_route_path(RedirecionamentoEnum.ERRO_404))
-
-	@staticmethod
-	def run():
-		run(host='localhost', port=8080, debug=True, reloader=True)
-
+if __name__ == '__main__':
+	run(host='localhost', port=8080, debug=True, reloader=True)
--- main.py
+++
@@ -1,4 +0,0 @@
-from app import Aplication
-
-if __name__ == '__main__':
-	Aplication.run()
